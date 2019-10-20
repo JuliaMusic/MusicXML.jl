913e44ef4a3dcb8433ea7b817325ab3642873057
aminya <aminyahyaabadi74@gmail.com>
2019-10-20 09:47:27 -0500

    example

---
+++ examples/examples.jl
@@ -0,0 +1,16 @@
+developing = true; # are you developing the package or just using?
+if developing
+# ] add Revise # if you don't have it
+    path = pwd()
+    if path[end-7:end] != "MusicXML"
+        error("cd(to the MusicXML path)")
+    end
+    push!(LOAD_PATH, ".")
+    using Revise
+    using MusicXML, EzXML, MIDI
+else
+    using MusicXML
+end
+################################################################
+
+data = readmusicxml(joinpath("examples", "musescore.musicxml"))
