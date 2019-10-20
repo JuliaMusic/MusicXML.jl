7157ff91f0c2c96cb1dcfad08dc4d0d90f60a58b
aminya <aminyahyaabadi74@gmail.com>
2019-10-20 03:46:02 -0500

    Scoreinstrument

--- src/MusicXML.jl
+++ src/MusicXML.jl
@@ -11,8 +11,24 @@ export readmusicxml, parsemusicxml
     midiinstrument
     midiinstrument(channel, program, volume, pan)
     midiinstrument()
+    Scoreinstrument
 
 midiinstrument type holds information about the sound of a midi instrument.
+The score-instrument type represents a single instrument within a score-part. As with the score-part type, each score-instrument has a required ID attribute, a name, and an optional abbreviation. A score-instrument type is also required if the score specifies MIDI 1.0 channels, banks, or programs. An initial midi-instrument assignment can also be made here. MusicXML software should be able to automatically assign reasonable channels and instruments without these elements in simple cases, such as where part names match General MIDI instrument names.
+"""
+mutable struct Scoreinstrument
+    name::String
+    ID::String
+    xml::Node
+end
+# xml constructor
+function Scoreinstrument(name,ID)
+    xml = ElementNode("score-instrument")
+    addelement!(xml, "instrument-name", string(name))
+    xml["id"] = ID * "-I1"
+    return Scoreinstrument(name, ID, xml)
+end
+################################################################
 
 # http://www.music-software-development.com/midi-tutorial.html - Part 4
 # Status byte : 1100 CCCC
