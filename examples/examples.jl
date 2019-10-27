f02bb20e4d3866fd55ba35230b88a450e5dced71
aminya <aminyahyaabadi74@gmail.com>
2019-10-27 09:06:29 -0500

    Simplify exmaples

--- examples/examples.jl
+++ examples/examples.jl
@@ -1,16 +1,7 @@
-developing = true; # are you developing the package or just using?
-if developing
-# ] add Revise # if you don't have it
-    path = pwd()
-    if path[end-7:end] != "MusicXML"
-        error("cd(to the MusicXML path)")
-    end
-    push!(LOAD_PATH, ".")
-    using Revise
-    using MusicXML
-else
-    using MusicXML
-end
+# ]dev path_to_MusicXML.j
+using Revise
+using MusicXML
+
 ################################################################
 
 data = readmusicxml(joinpath("examples", "musescore.musicxml"))
