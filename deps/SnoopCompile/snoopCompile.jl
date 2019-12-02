9645dabf109bb2802c5b3e60d8fa7303122fcb22
aminya <aminyahyaabadi74@gmail.com>
2019-12-02 00:35:23 -0600

    Adding SnoopCompile

---
+++ deps/SnoopCompile/snoopCompile.jl
@@ -0,0 +1,27 @@
+using SnoopCompile
+
+cd(@__DIR__)
+################################################################
+
+### Log the compiles
+# This only needs to be run once (to generate log file)
+
+SnoopCompile.@snoopc "$(pwd())/Snoop.log" begin
+
+    # Use runtests.jl or your exmaples that uses package:
+
+    using MusicXML, Pkg
+
+    include(joinpath(dirname(dirname(pathof(MusicXML))), "examples","examples.jl"))
+
+end
+
+################################################################
+
+### Parse the compiles and generate precompilation scripts
+# This can be run repeatedly to tweak the scripts
+
+data = SnoopCompile.read("$(pwd())/Snoop.log")
+
+pc = SnoopCompile.parcel(reverse!(data[2]))
+SnoopCompile.write("$(pwd())/precompile", pc)
