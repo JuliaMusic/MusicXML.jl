e3689a94730f0513bb584b7aff7b60a0180aa49e
aminya <aminyahyaabadi74@gmail.com>
2020-03-01 02:29:55 -0600

    parsing snoopcompile

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -3,5 +3,5 @@ using SnoopCompile
 @snoopi_bot "MusicXML" begin
   using MusicXML
   examplePath = joinpath(dirname(dirname(pathof(MusicXML))), "examples")
-  include(joinpath(examplePath, "examples.jl"))
+  include(joinpath(examplePath, "parsing.jl"))
 end
