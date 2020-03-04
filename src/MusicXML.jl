ec7f2213a37efc54f3b912d3e3aaa10cf3dcba10
aminya <aminyahyaabadi74@gmail.com>
2020-03-03 23:48:42 -0600

    @MX docstring

--- src/MusicXML.jl
+++ src/MusicXML.jl
@@ -4,6 +4,38 @@ module MusicXML
 export MX
 const MX = MusicXML
 
+# Type Helper Macro
+export @MX
+"""
+    @MX
+
+A macro that adds `MX.` to the name of the MusicXML types.
+
+Since MusicXML's types are not exported from the package to avoid conflicts with the similarly named types from other libraries (such as `Dates.Time`, `MIDI.Note`), `@MX` is given that facilitates using MusicXML types.
+
+`@MX` assumes that all the types with these kinds of names inside in front of a macro, between `()`, or between `begin end` are MusicXML types unless explicitly written (e.g. `MIDI.Note`).
+
+# Examples
+```julia
+@MX begin
+    Note(pitch = Pitch(step = "C", alter = 0, octave = 4), duration =  4))
+    # all the code ...
+end
+```
+is translated to:
+```julia
+    MX.Note(pitch = Pitch(step = "C", alter = 0, octave = 4), duration =  4))
+```
+
+Type is untouched if it is called explicitly from another library:
+```julia
+@MX Dates.Time(12,53,40)
+```
+remains as:
+```julia
+Dates.Time(12,53,40)
+```
+"""
 macro MX(expr)
     expr_string = string(expr)
     expr_string = reduce(replace,
